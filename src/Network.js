4b4aea751cc393545346e9a0df3a38bc746633ac
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-09-13 03:35:49 +0100

    Update ****

--- src/Network.js
+++ src/Network.js
@@ -1,3 +1,3 @@
 // export const baseUrl =  "http://localhost:3000";
-export const baseUrl = "https://ryd-learning-api-v2.onrender.com"
-// export const baseUrl =  "https://api-pro.rydlearning.com";
+// export const baseUrl = "https://ryd-learning-api-v2.onrender.com"
+export const baseUrl =  "https://api-pro.rydlearning.com";
